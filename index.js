7a9eca8a48b4482196a26f62c3c2dd36ec046d03
Joseph Sutera <joesutera11@gmail.com>
2022-07-26 00:42:18 -0400

    commit tree 42338be5d53a0634610e73959ee47e98d3d96cce

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+
+let car =document.getElementById('main')
+car.remove()
+let newHeader= document.createElement('h1')
+newHeader.id="victory"
+newHeader.textContent="YOUR-NAME is the champion"
